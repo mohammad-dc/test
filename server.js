dd8ce97d611c7065e14ecfe26d2de56f83fd83e3
Mohammad Ahmad <ma3571318z@gmail.com>
2021-05-27 11:18:01 +0300

    1st commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const app = require('express')();
+const dotenv = require('dotenv').config();
 
 app.get('/hello', function(req, res, next){res.send('<h1>Hi</h1>')})
-app.listen(3000, () => console.log('object'))
+app.listen(process.env.PORT, () => console.log('object'))
